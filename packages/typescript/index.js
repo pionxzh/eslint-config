ed1f4db858d68a261fec1532bdd54b041762f7d3
Pionxzh <spigbbbbb@gmail.com>
2023-03-26 17:11:46 +0800

    feat(typescript): prefer top level type import

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -63,12 +63,13 @@ module.exports = {
     ),
     rules: {
         'import/named': 'off',
+        'import/consistent-type-specifier-style': ['error', 'prefer-top-level'], // prefer import type { T } from '...' than import { type T } from '...'
 
         // TS
         '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
         '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
         '@typescript-eslint/type-annotation-spacing': ['error', {}],
-        '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+        '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'separate-type-imports', disallowTypeAnnotations: false }],
         '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
         '@typescript-eslint/prefer-ts-expect-error': 'error',
 
