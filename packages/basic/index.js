465b327894c69bbf2abab291b361f992accb6fdc
Pionxzh <spigbbbbb@gmail.com>
2023-03-26 17:01:20 +0800

    feat: sort import order alphabetize

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -200,7 +200,13 @@ module.exports = {
     ],
     rules: {
         // import
-        'import/order': 'error',
+        'import/order': ['error', {
+            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+            alphabetize: {
+                order: 'asc',
+                caseInsensitive: true,
+            },
+        }],
         'import/first': 'error',
         'import/no-mutable-exports': 'error',
         'import/no-unresolved': 'off',
