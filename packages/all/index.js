3736ff031d9400225034559e1480b7848cdef802
Pionxzh <pionxzh@csie.io>
2023-10-05 20:09:32 +0800

    feat: follow changes from @antfu/eslint-config v0.41.0~v1.0.0-beta.20
    eslint flat configuration

--- packages/all/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    extends: [
-        '@pionxzh/eslint-config-ts',
-    ],
-}
---
+++ src/configs/imports.ts
@@ -0,0 +1,36 @@
+import type { FlatESLintConfigItem, OptionsStylistic } from '../types'
+import { pluginAntfu, pluginImport } from '../plugins'
+
+export function imports(options: OptionsStylistic = {}): FlatESLintConfigItem[] {
+  const {
+    stylistic = true,
+  } = options
+
+  return [
+    {
+      name: 'pionxzh:imports',
+      plugins: {
+        import: pluginImport,
+        pionxzh: pluginAntfu,
+      },
+      rules: {
+        'import/first': 'error',
+        'import/no-duplicates': 'error',
+        'import/no-mutable-exports': 'error',
+        'import/no-named-default': 'error',
+        'import/no-self-import': 'error',
+        'import/no-webpack-loader-syntax': 'error',
+        'import/order': 'error',
+
+        'pionxzh/import-dedupe': 'error',
+        'pionxzh/no-import-node-modules-by-path': 'error',
+
+        ...stylistic
+          ? {
+              'import/newline-after-import': ['error', { considerComments: true, count: 1 }],
+            }
+          : {},
+      },
+    },
+  ]
+}
